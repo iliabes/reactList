033c32d958cdde263aa70527327087285eab8ea0
Forgetfull <oksana14888@gmail.com>
2021-02-06 16:52:58 +0500

    10m about + home pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter, Switch, Route} from "react-router-dom"
+import {About} from './pages/about'
+import {Home} from './pages/home'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <div className='container pt-4'>
+      <Switch>
+      <Route path={'/'} exact component={Home}/>
+      <Route path={'/about'} component={About}/>
+      </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
