d4161f6dde7910afd704cf54b209b5836d8de2d8
iliabes <oksana14888@gmail.com>
2021-02-17 23:37:16 +0300

    add date and button del 25min

--- src/components/notes.js
+++ src/components/notes.js
@@ -4,8 +4,8 @@ export const Notes = (notes) => {
     return(
         <ul className='list-group'>
         {notes.notes.map((item) => (
-              <li className='list-group-item'>{item}<button>1</button></li>
-              
+            <li className='list-group-item'>{item}<p>{new Date().toLocaleDateString()}</p><button>'del'</button></li>
+
         ))}
         
         </ul>
